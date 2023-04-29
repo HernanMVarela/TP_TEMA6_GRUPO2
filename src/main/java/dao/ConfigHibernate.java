7153f0760c89834aaa06cccdef94e3eac7a4bdd7
HernanMVarela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-04-29 19:53:46 -0300

    Initial commit

---
+++ src/main/java/dao/ConfigHibernate.java
@@ -0,0 +1,20 @@
+package dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class ConfigHibernate {
+
+	private SessionFactory sessionFactory;
+	private Session session;
+	
+	public ConfigHibernate() {
+		Configuration configuration = new Configuration();
+    	configuration.configure();
+    	ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+    	this.sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	}
+}
