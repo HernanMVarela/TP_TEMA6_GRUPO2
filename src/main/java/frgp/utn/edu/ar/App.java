8fde990a39c781a739cba910def9825f5eba1924
LeonardoDFX <128710684+LeonardoDFX@users.noreply.github.com>
2023-05-01 20:16:18 -0300

    App
    Pequeño update agregando lo hecho en biblioteca para comprobar que todo funciona, así queda de ejemplo por las dudas.

--- src/main/java/frgp/utn/edu/ar/App.java
+++ src/main/java/frgp/utn/edu/ar/App.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 
 import frgp.utn.edu.ar.dao.ConfigHibernate;
 import frgp.utn.edu.ar.entidad.Autor;
+import frgp.utn.edu.ar.entidad.Biblioteca;
 
 /**
  * Hello world!
@@ -14,7 +15,7 @@ public class App
     public static void main( String[] args )
     {
         
-        Autor autor = new Autor();
+    	Autor autor = new Autor();
     	autor.setNombre("Pepe");
     	autor.setApellido("123");
     	autor.setEmail("sdsd");
@@ -22,11 +23,17 @@ public class App
     	autor.setId("1");
         
         ConfigHibernate ch = new ConfigHibernate();
-		Session session= ch.abrirConexion();
+        Session session= ch.abrirConexion();
 		
-	        
 	    session.beginTransaction();
 	    session.save(autor);
+	    
+	    Biblioteca libroFisico = new Biblioteca(7,"Malo");
+	    session.save(libroFisico);
+	    System.out.println(libroFisico.toString());
+	    libroFisico= new Biblioteca(3,"De 10");
+	    session.save(libroFisico);
+	    System.out.println(libroFisico.toString());
 	    
 	    session.getTransaction().commit();    
 		ch.cerrarSession();
