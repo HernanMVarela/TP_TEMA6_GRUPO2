8ba322734524a6d0045c5a76e6bbc6b37133085a
HernanMVarela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-05-14 18:22:45 -0300

    14-05-2023 | 18:21
    - Agrego método punto_2 al Dao de biblioteca con su consulta
    - Prueba de funcionamiento directo desde el main

--- src/main/java/frgp/utn/edu/ar/TP4.java
+++ src/main/java/frgp/utn/edu/ar/TP4.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.hibernate.Session;
 
 import frgp.utn.edu.ar.dao.ConfigHibernate;
+import frgp.utn.edu.ar.dao.DaoHibernateBiblioteca;
+import frgp.utn.edu.ar.entidad.Biblioteca;
 import frgp.utn.edu.ar.entidad.Libro;
 
 public class TP4 {
 
 	public static void main(String[] args) {
 		
+		
+		/*
 		ConfigHibernate ch = new ConfigHibernate();
 		Session session = ch.abrirConexion();
 		
@@ -21,6 +25,17 @@ public class TP4 {
 		}
 		
 		ch.cerrarSession();
+		
+		*/
+		
+		List<Biblioteca> lista = DaoHibernateBiblioteca.punto_2();
+		
+		for (Biblioteca biblioteca : lista) {
+			System.out.println(" ID: " + biblioteca.getIdBiblioteca() + 
+					   " | Fecha: " + biblioteca.getFechadealta().toString() + 
+					   " | Estado: " + biblioteca.getEstado());
+		}
+		
 	}
 
 }
--- src/main/java/frgp/utn/edu/ar/dao/DaoHibernateBiblioteca.java
+++ src/main/java/frgp/utn/edu/ar/dao/DaoHibernateBiblioteca.java
@@ -1,12 +1,47 @@
 package frgp.utn.edu.ar.dao;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
 
 import frgp.utn.edu.ar.entidad.Biblioteca;
+import frgp.utn.edu.ar.entidad.EEstadoBiblioteca;
 
 public class DaoHibernateBiblioteca {
+	
+	public static List<Biblioteca> punto_2() {
+		try {
+			ConfigHibernate ch = new ConfigHibernate();
+			Session session = ch.abrirConexion();
+		
+			session.beginTransaction();
+			@SuppressWarnings("unchecked")
+			List<Object[]> fullList = (List<Object[]>)session.createQuery("SELECT bib.idBiblioteca, "
+																		+ "bib.fechadealta, "
+																		+ "bib.estado "
+																		+ "FROM Biblioteca as bib "
+																		+ "WHERE bib.estado=1").list();
+			session.getTransaction().commit();
+			ch.cerrarSession();
+			
+			List<Biblioteca> bibliotecas = new ArrayList<Biblioteca>();
+			
+			for (Object[] objects : fullList) {
+				Biblioteca biblio = new Biblioteca();
+				biblio.setIdBiblioteca((Integer)objects[0]);
+				biblio.setFechadealta((Date)objects[1]);
+				biblio.setEstado((EEstadoBiblioteca)objects[2]);
+
+				bibliotecas.add(biblio);
+			}
+			return bibliotecas;
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return null;
+		}
+	}	
 	
 	public static void AddBiblioteca(Biblioteca biblio) {
 		try {
